d003b6b91994026cc7bedab5c098968cc0568c44
Simone-Albero <simone.albero29@gmail.com>
2024-05-18 21:51:12 +0200

    Implementation of Dataset

---
+++ src/NIDS_Framework/dataset.py
@@ -0,0 +1,80 @@
+from typing import List, Optional
+import os
+
+import pandas as pd # dependences: pandas?
+
+from utilities import CacheUtils
+
+class DatasetProperties:
+    __slots__ = ['features', 'categorical_features', 'labels_column', 'benign_label']
+
+    def  __init__(self, features: List[str], categorical_features: List[str], labels_column: Optional[str] = None, benign_label: Optional[str] = None) -> None:
+        """
+        Defines the format of specific dataset.
+
+        Params:
+            - features: fields to include as part of the training.
+            - categorical_features: fields that should be treated as categorical.
+            - labels_column: fields treated as class attributes.
+            - benign_label: label of benign traffic.
+        """
+        self.features: List[str] = features
+        self.categorical_features: List[str] = categorical_features
+        self.labels_column: Optional[str] = labels_column
+        self.benign_label: Optional[str] = benign_label
+
+class Dataset:
+    __slots__ = ['cache_path', '_df', '_properties']
+
+    def __init__(self, cache_path: str, dataset_path: Optional[str] = None, properties: Optional[DatasetProperties] = None) -> None:
+        """
+        An abstraction for datasets.
+
+        Params:
+            - cache_path: the path where the cache file should be stored.
+            - dataset_path: the path to the dataset file (optional).
+            - properties: properties of the dataset (optional).
+        """
+        self.cache_path: str = cache_path
+        self._df: Optional[pd.DataFrame] = None
+        self._properties: Optional[DatasetProperties] = None
+
+        if CacheUtils.exists(cache_path):
+            self._load_from_cache()
+        
+        elif dataset_path is not None:
+            self._load_from_file(dataset_path, properties)
+        
+        else:
+            raise Exception(f"Please provide either the cache path or the dataset path!")
+
+    def _load_from_file(self, dataset_path: str, properties: DatasetProperties) -> None:
+        """
+        Load dataset from file and save to cache.
+
+        Params:
+            - dataset_path: the path to the dataset file.
+            - properties: properties of the dataset.
+        """
+        if os.path.exists(dataset_path):
+            print(f"Reading dataset from path '{dataset_path}' ...")
+            if dataset_path.lower().endswith(".csv"):
+                self._df = pd.read_csv(dataset_path)
+                self._properties = properties
+                self._save_to_cache()
+            else:
+                raise NotImplementedError("Only CSV files are supported.")
+
+        else:
+            raise FileNotFoundError(f"Dataset file not found: {dataset_path}")
+
+    def _save_to_cache(self) -> None:
+        """Save DataFrame and properties to cache."""
+        data = {'df': self._df, 'properties': self._properties}
+        CacheUtils.write(self.cache_path, data)
+
+    def _load_from_cache(self) -> None:
+        """Load DataFrame and properties from cache."""
+        data = CacheUtils.read(self.cache_path)
+        self._df, self._properties = data['df'], data['properties']
+        
