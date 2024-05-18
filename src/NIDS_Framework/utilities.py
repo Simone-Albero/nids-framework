b1e1f75fa8390f84b984b22eec318e12b5f620b5
Simone-Albero <simone.albero29@gmail.com>
2024-05-18 21:50:15 +0200

    Implementation of CacheUtils

---
+++ src/NIDS_Framework/utilities.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+
+class CacheUtils(object):
+    @staticmethod
+    def exists(cache_path: str):
+        return os.path.exists(cache_path)
+        
+    @staticmethod
+    def read(cache_path: str):
+        print(f"Reading directly from cache '{cache_path}' ...")
+        try:
+            with open(cache_path, 'rb') as r:
+                return pickle.load(r)
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"File not found: {cache_path}") from e
+        except pickle.UnpicklingError as e:
+            raise pickle.UnpicklingError("Error unpickling data.") from e
+
+    @staticmethod
+    def write(cache_path: str, data):
+        print(f"Writing to cache '{cache_path}' ...")
+        try:
+            with open(cache_path, 'wb') as file:
+                pickle.dump(data, file)
+        except Exception as e:
+            print(f"Error writing to file: {e}")
