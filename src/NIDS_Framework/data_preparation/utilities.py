66f688a973b5e5b187f5be07bbd5ca291a4a93ff
Simone-Albero <simone.albero29@gmail.com>
2024-06-07 15:28:07 +0200

    some update on data_preparation utilities.

--- src/NIDS_Framework/data_preparation/utilities.py
+++ src/NIDS_Framework/data_preparation/utilities.py
@@ -1,14 +1,46 @@
 from typing import Dict, Any, Tuple
 import logging
 
-import torch
 import pandas as pd
+import numpy as np
+import torch
 
 from data_preparation.processor import DatasetProperties
 
 
+def base_pre_processing(
+    dataset: pd.DataFrame, properties: DatasetProperties, bound: int = 1000000
+) -> None:
+    logging.debug(f"Bounding numeric features to {bound}...")
+    for col in properties.numeric_features:
+        column_values = dataset[col]
+        column_values[~np.isfinite(column_values)] = 0
+        column_values[column_values < -bound] = 0
+        column_values[column_values > bound] = 0
+        dataset[col] = column_values.astype("float32")
+
+
+def log_pre_processing(
+    dataset: pd.DataFrame, properties: DatasetProperties, bound: int = 1000000
+) -> None:
+    logging.debug("Normalizing numeric features with Log...")
+    for col in properties.numeric_features:
+        column_values = dataset[col]
+        min_value = np.min(column_values)
+        max_value = np.max(column_values)
+        gap = max_value - min_value
+
+        if gap == 0:
+            dataset[col] = np.zeros_like(column_values, dtype="float32")
+        else:
+            column_values -= min_value
+            column_values = np.log(column_values + 1)
+            column_values *= 1.0 / np.log(gap + 1)
+            dataset[col] = column_values
+
+
 def categorical_pre_processing(
-    dataset: pd.DataFrame, properties: DatasetProperties, categorical_levels=32
+    dataset: pd.DataFrame, properties: DatasetProperties, categorical_levels: int = 32
 ) -> None:
     logging.debug(
         f"Mapping {len(properties.categorical_features)} categorical features to {categorical_levels} numeric tags..."
@@ -21,7 +53,9 @@ def categorical_pre_processing(
         dataset[col] = dataset[col].apply(lambda x: value_map.get(x, -1) + 1)
 
 
-def bound_transformation(sample: Dict[str, Any], bound: int) -> Dict[str, Any]:
+def bound_transformation(
+    sample: Dict[str, Any], bound: int = 1000000
+) -> Dict[str, Any]:
     features, stats = sample["data"], sample["stats"]
     features = torch.clamp(features, min=-bound, max=bound)
     stats["min"] = torch.clamp(stats["min"], min=-bound)
@@ -52,25 +86,9 @@ def log_transformation(sample: Dict[str, Any]) -> Dict[str, Any]:
     return sample
 
 
-# Deprecated
-def categorical_value_encoding(
-    sample: Dict[str, Any], categorical_bound: int
+def one_hot_encoding(
+    sample: Dict[str, Any], categorical_levels: int = 32
 ) -> Dict[str, Any]:
-    features, stats = sample["data"], sample["stats"]
-    categorical_levels = stats["categorical_levels"][:, :(categorical_bound - 1)].t()
-
-    value_encoding = torch.zeros_like(features)
-    for col_idx, col in enumerate(features.t()):
-        for row_idx, val in enumerate(col):
-            mask = (categorical_levels[:, col_idx] == val).nonzero()
-            if mask.numel() > 0:
-                value_encoding[row_idx, col_idx] = mask.item() + 1
-
-    sample["data"] = value_encoding
-    return sample
-
-
-def one_hot_encoding(sample: Dict[str, Any], categorical_levels: int) -> Dict[str, Any]:
     features = sample["data"]
 
     one_hot = torch.nn.functional.one_hot(features, num_classes=categorical_levels)
@@ -81,4 +99,22 @@ def one_hot_encoding(sample: Dict[str, Any], categorical_levels: int) -> Dict[st
     elif len(one_hot.shape) > 2:
         sample["data"] = one_hot.view(one_hot.size(0), -1)
 
+    return sample
+
+
+# Deprecated
+def categorical_value_encoding(
+    sample: Dict[str, Any], categorical_bound: int = 32
+) -> Dict[str, Any]:
+    features, stats = sample["data"], sample["stats"]
+    categorical_levels = stats["categorical_levels"][:, : (categorical_bound - 1)].t()
+
+    value_encoding = torch.zeros_like(features)
+    for col_idx, col in enumerate(features.t()):
+        for row_idx, val in enumerate(col):
+            mask = (categorical_levels[:, col_idx] == val).nonzero()
+            if mask.numel() > 0:
+                value_encoding[row_idx, col_idx] = mask.item() + 1
+
+    sample["data"] = value_encoding
     return sample
