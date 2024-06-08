16eeea53def4a80702facb6434b03dfa43f7e812
Simone-Albero <simone.albero29@gmail.com>
2024-06-08 18:51:30 +0200

    improved monitoring stuf.

--- src/NIDS_Framework/utilities.py
+++ src/NIDS_Framework/utilities.py
@@ -16,26 +16,31 @@ def collect_stats(
 ) -> None:
     process = psutil.Process(pid)
     start_time = time.time()
-    start_cpu_usage = psutil.cpu_percent(interval=0.01)
 
     exec_times = []
     global_cpu_usages = []
     proc_cpu_usages = []
-    memory_usages = []
+    rss_memory_usages = []
+    vms_memory_usages = []
+
+    # starting_cpu_usage = psutil.cpu_percent(interval=0.1)
 
     while not stop_event.is_set():
         current_time = time.time() - start_time
-        global_cpu_usage = psutil.cpu_percent(interval=0.01) - start_cpu_usage
-        proc_cpu_usage = process.cpu_percent(interval=0.01)
-        memory_usage = process.memory_info().rss / 1024 / 1024
+        global_cpu_usage = psutil.cpu_percent(interval=0.1) # - starting_cpu_usage
+        proc_cpu_usage = process.cpu_percent(interval=0.1)
+        memory_usage = process.memory_info()
+        rss_memory_usage = memory_usage.rss / 1024 / 1024
+        vms_memory_usage = memory_usage.rss / 1024 / 1024
 
         exec_times.append(current_time)
         global_cpu_usages.append(global_cpu_usage)
         proc_cpu_usages.append(proc_cpu_usage)
-        memory_usages.append(memory_usage)
+        rss_memory_usages.append(rss_memory_usage)
+        vms_memory_usages.append(vms_memory_usage)
 
         time.sleep(interval)
-    stats_queue.put((exec_times, global_cpu_usages, proc_cpu_usages, memory_usages))
+    stats_queue.put((exec_times, global_cpu_usages, proc_cpu_usages, rss_memory_usages, vms_memory_usages))
 
 
 def trace_stats(
@@ -61,20 +66,21 @@ def trace_stats(
                 stop_event.set()
                 process.join()
 
-                exec_times, global_cpu_usage, proc_cpu_usages, memory_usages = stats_queue.get()
+                exec_times, global_cpu_usage, proc_cpu_usages, rss_memory_usages, vms_memory_usages = stats_queue.get()
                 exec_time = exec_times[-1]
                 med_gloabl_cpu_usage = statistics.median(global_cpu_usage)
                 med_proc_cpu_usage = statistics.median(proc_cpu_usages)
-                med_memory_usage = statistics.median(memory_usages)
+                med_rss_memory_usage = statistics.median(rss_memory_usages)
+                med_vms_memory_usage = statistics.median(vms_memory_usages)
 
                 if not os.path.exists(file_path) or reset_logs:
                     os.makedirs(os.path.dirname(file_path), exist_ok=True)
                     with open(file_path, "w") as f:
-                        f.write(f"FUNCTION,EXEC_TIME,GLOBAL_CPU_USAGE,PROC_CPU_USAGE,MEMORY_USAGE\n")
+                        f.write(f"FUNCTION,EXEC_TIME,GLOBAL_CPU_USAGE,PROC_CPU_USAGE,PROC_RSS_MEMORY_USAGE,PROC_VMS_MEMORY_USAGE\n")
 
                 with open(file_path, "a") as f:
                     f.write(
-                        f"{func.__name__},{exec_time:.2f},{med_gloabl_cpu_usage:.2f},{med_proc_cpu_usage:.2f},{med_memory_usage:.2f}\n"
+                        f"{func.__name__},{exec_time:.2f},{med_gloabl_cpu_usage:.2f},{med_proc_cpu_usage:.2f},{med_rss_memory_usage:.2f},{med_vms_memory_usage:.2f}\n"
                     )
                 logging.info("Function '%s' finished.", func.__name__)
 
