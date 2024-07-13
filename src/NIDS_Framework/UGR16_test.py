fc895056657f62c7bde88a4d263d6fd083aff2c7
Albero <Albero@node0.albero-211239.idsbenchmark-pg0.wisc.cloudlab.us>
2024-07-13 15:59:26 +0000

    stuff

--- src/NIDS_Framework/UGR16_test.py
+++ src/NIDS_Framework/UGR16_test.py
@@ -10,7 +10,6 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import random
 
-
 def ms_stamp_update():
     dataset_path = "dataset/UGR16/custom/fixed_test.csv"
     df = pd.read_csv(dataset_path)
@@ -21,89 +20,6 @@ def ms_stamp_update():
     df = df.sort_values(by='te')
 
     df.to_csv('dataset/UGR16/custom/ms_test.csv', index=False)
-
-def fixed_windows_dataset():
-    dataset_path = "dataset/UGR16/custom/ms_test.csv"
-    df = pd.read_csv(dataset_path)
-    df['te'] = pd.to_datetime(df['te'])
-
-    WINDOW_SIZE_MS = pd.Timedelta(milliseconds=10)
-    MAX_WINDOW = 64
-    last_stamp = df.iloc[-1]['te']
-    curr_stamp = df.iloc[0]['te']
-
-    dfs = {1: [], 2: [], 4: [], 8: [], 16: [], 32: [], 64: []}
-
-    i = 0
-    while curr_stamp < last_stamp:
-        curr_window = MAX_WINDOW
-
-        if i + curr_window >= len(df):
-            break
-
-        while True:
-            if (df.iloc[i + curr_window]['te'] - curr_stamp <= WINDOW_SIZE_MS) or curr_window == 1:
-                break
-            else: 
-                curr_window //= 2
-        
-        dfs[curr_window].append(df.iloc[i:i + curr_window])
-        i += curr_window
-        # i += 1
-        curr_stamp = df.iloc[i]['te']
-        print(f"Progress: {curr_stamp} / {last_stamp} : [{i}/{len(df)}]", end='\r')
-
-    for window_size, df_list in dfs.items():
-        if df_list:
-            pd.concat(df_list).to_csv(f"dataset/UGR16/custom/{window_size}_test.csv", index=False)
-            print(f"{window_size}:{len(df_list)}")
-
-    print(f"Tot: {len(df)}")
-
-def proportion_analysis():
-    dataset_path = "dataset/UGR16/fixed/8_fixed.csv"
-    df = pd.read_csv(dataset_path)
-
-    attack_mask = df["label"] == "dos"
-    attack_num = attack_mask.sum()
-    proportion = attack_num / len(df)
-    print(proportion)
-
-def proportion_fix():
-    P = 0.0185
-    WINDOW_SIZE = 8
-
-    dataset_path = f"dataset/UGR16/custom/{WINDOW_SIZE}_test_l.csv"
-    df = pd.read_csv(dataset_path)
-
-    print("Generating blocks...")
-    positive_blocks = []
-    negative_blocks = []
-    for i in tqdm(range(0, len(df), WINDOW_SIZE)):
-        block = df.iloc[i:i + WINDOW_SIZE]
-        if block.iloc[-1]["label"] == "dos":
-            positive_blocks.append(block)
-        else:
-            negative_blocks.append(block)
-
-    random.shuffle(negative_blocks)
-
-    num_ones = len(positive_blocks)
-    num_negatives_to_keep = int(num_ones / P)
-    print(num_ones, num_negatives_to_keep)
-
-    negative_blocks = negative_blocks[:num_negatives_to_keep]
-    
-    print("Concatenating blocks...")
-    df_positive = pd.concat(positive_blocks)
-    df_negative = pd.concat(negative_blocks)
-
-    df_fixed = pd.concat([df_positive, df_negative]).reset_index(drop=True)
-    print(len(df_fixed))
-    output_path = f"dataset/UGR16/fixed/{WINDOW_SIZE}_test_fixed.csv"
-    df_fixed.to_csv(output_path, index=False)
-
-
 
 def fixed_windows_analisys():
     dataset_path = "dataset/UGR16/custom/ms_train.csv"
@@ -151,90 +67,33 @@ def fixed_windows_analisys():
     plt.savefig('saves/window_gaps.png', bbox_inches='tight')
     plt.show()
 
-def fixed_gap_analisys():
+def fixed_windows_dataset():
     dataset_path = "dataset/UGR16/custom/ms_train.csv"
     df = pd.read_csv(dataset_path)
     df['te'] = pd.to_datetime(df['te'])
 
-    WINDOW_SIZE_MS = 10
-    last_stamp = df.iloc[-1]['te']
-    curr_stamp = df.iloc[0]['te']
+    WINDOW_SIZE_MS = pd.Timedelta(milliseconds=10)
+    MAX_WINDOW = 64
+    MIN_WINDOW = 2
 
-    window_occurrences = {}
+    dfs = {1: [], 2: [], 4: [], 8: [], 16: [], 32: [], 64: []}
 
-    i = 0
-    while curr_stamp < last_stamp:
-        gap = curr_stamp + pd.to_timedelta(WINDOW_SIZE_MS, unit='ms')
-        occurrences = 0
+    for i in tqdm(range(len(df))):
+        curr_window = MIN_WINDOW
+        curr_stamp = df.iloc[i]['te']
 
-        while i < len(df) and df.iloc[i]['te'] < gap:
-            occurrences += 1
-            i += 1
-            if i < len(df):
-                curr_stamp = df.iloc[i]['te']
-            else:
-                break
+        if i + MAX_WINDOW >= len(df):
+            break
 
-        if occurrences not in window_occurrences:
-            window_occurrences[occurrences] = 0
+        while df.iloc[i + curr_window - 1]['te'] - curr_stamp <= WINDOW_SIZE_MS and curr_window <= MAX_WINDOW:
+            dfs[curr_window].append(i + curr_window - 1)
+            curr_window *= 2
 
-        window_occurrences[occurrences] += 1
-
-        print(f"curr_stamp: {curr_stamp}, last_stamp: {last_stamp}", end='\r')
-
-    with open('saves/window_occurrences.pkl', 'rb') as file:
-        window_occurrences = pickle.load(file)
-
-    xs = sorted(window_occurrences.keys())[:100]
-    ys = [window_occurrences[x] for x in xs]
-    indices = list(range(len(xs)))
-
-
-    plt.figure(figsize=(20, 10))
-    plt.bar(indices, ys)
-    plt.xlabel('Record Number')
-    plt.ylabel('Occurrences')
-    plt.title('Distribution of Occurrences for 10ms Window')
-    plt.xticks(indices, xs, rotation=90)
-    plt.tight_layout()
-
-    plt.savefig('saves/fixed_window_10ms.png', bbox_inches='tight')
-    plt.show()
-
-
-def time_stamps_analisys():
-    dataset_path = "dataset/UGR16/custom/train.csv"
-    df = pd.read_csv(dataset_path)
-    time_stamps = df['te'].value_counts()
-    time_stamps = time_stamps.sort_index()
-
-    xs = pd.to_datetime(time_stamps.index)
-    ys = time_stamps.values
-
-
-    plt.figure(figsize=(20, 10))
-    plt.plot_date(xs, ys, linestyle='-')
-    plt.xlabel('Time Stamp')
-    plt.ylabel('Occurrences')
-    plt.title('Occurrences for Time Stamps')
-    plt.grid(True)
-    plt.savefig('saves/time_stamps.png', bbox_inches='tight')
-    plt.show()
-
-    occurrences_count = time_stamps.value_counts().sort_index()
-    print(occurrences_count)
-    
-    xs = occurrences_count.index.astype(int)
-    ys = occurrences_count.values
-    plt.figure(figsize=(20, 10))
-    plt.bar(xs, ys)
-    plt.xlabel('Sizes')
-    plt.ylabel('Occurrences')
-    plt.title('Sizes Occurrences')
-    plt.tight_layout()
-
-    plt.savefig('saves/occurrences_count.png', bbox_inches='tight')
-    plt.show()
+    for window_size, df_list in dfs.items():
+        if df_list:
+            indices_df = pd.DataFrame(df_list, columns=['index'])
+            indices_df.to_csv(f"dataset/UGR16/fixed/{window_size}_test.csv", index=False)
+            print(f"{window_size}: {len(df_list)}")
 
 def main():
     debug_level = logging.INFO
@@ -243,7 +102,7 @@ def main():
         format="%(message)s",
         handlers=[RichHandler(rich_tracebacks=True, show_time=False, show_path=False)],
     )
-    proportion_fix()
+    fixed_windows_dataset()
 
 
 if __name__ == "__main__":
